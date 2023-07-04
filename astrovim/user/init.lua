f8e10b40b5cb17874401984c639c8a31a9360e59
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2023-07-04 02:03:10 -0400

    Added todo comments nvim config

--- astrovim/user/init.lua
+++ astrovim/user/init.lua
@@ -1,26 +1,27 @@
 return {
-  plugins = {
-    "AstroNvim/astrocommunity",
-    { import = "astrocommunity.colorscheme.catppuccin" },
-    { import = "astrocommunity.utility.transparent-nvim" },
-    { import = "astrocommunity.pack.cpp" },
-    { import = "astrocommunity.pack.python" },
-    { import = "astrocommunity.pack.rust" },
-    { import = "astrocommunity.pack.lua" },
-    { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  },
-  lsp = {
-    config = {
-      rust_analyzer = {
-        settings = {
-          ["rust-analyzer"] = {
-            cargo = {
-              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
-              extraArgs = { "--profile", "rust-analyzer" },
-            },
-          },
-        },
-      },
-    },
-  },
+	plugins = {
+		"AstroNvim/astrocommunity",
+		{ import = "astrocommunity.colorscheme.catppuccin" },
+		{ import = "astrocommunity.utility.transparent-nvim" },
+		{ import = "astrocommunity.pack.cpp" },
+		{ import = "astrocommunity.pack.python" },
+		{ import = "astrocommunity.pack.rust" },
+		{ import = "astrocommunity.pack.lua" },
+		{ import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+		{ import = "astrocommunity.editing-support.todo-comments-nvim" },
+	},
+	lsp = {
+		config = {
+			rust_analyzer = {
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+							extraArgs = { "--profile", "rust-analyzer" },
+						},
+					},
+				},
+			},
+		},
+	},
 }
