e8b0c67a8a58c9139c3e5c37a5284f27749b9c16
JonathanJT109 <jonathangon.2014@gmail.com>
2023-07-04 01:30:42 -0400

    Add AstroVim user config

---
+++ astrovim/user/init.lua
@@ -0,0 +1,26 @@
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" },
+    { import = "astrocommunity.utility.transparent-nvim" },
+    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  },
+  lsp = {
+    config = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+              extraArgs = { "--profile", "rust-analyzer" },
+            },
+          },
+        },
+      },
+    },
+  },
+}
